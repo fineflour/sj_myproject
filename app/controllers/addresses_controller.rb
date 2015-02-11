53752de6637972d2bcaeef16604eb807e42ebbfc
fineflour <soojin.cho@bfa.org>
2015-02-11 14:56:07 -0800

    multiple CRUD added

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -7,29 +7,87 @@ class AddressesController < ApplicationController
   end
 
   def create
-    @address = Address.new(address_params)
-              if @address.save
-                   flash[:success] = "New Address has been added!"
-     redirect_to lists_path 
-             else
-                  render 'new'
-             end
+    user = User.find(params[:user_id])
+    @address = user.address.create(address_params)
+
+      respond_to do |format|
+          if @address.save
+              format.html { redirect_to([@address.user, @address], flash[:success] => "New Address has been added!")}
+              format.xml  { render :xml => @comment, :status => :created, :location => [@comment.post, @comment] }
+         else
+              format.html { render :action => "new" }
+              format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
+         end
+      end
   end
 
   def index
-    @addresses = Address.all.order('id desc')
+    #@addresses = Address.all.order('id desc')
+    user = User.find(params[:user_id])
+    @addresses = user.address
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @comment }
+    end
   end
 
   def show
-    @addresses = Address.find_by_user_id(current_user.id)
+    user = User.find(params[:user_id])
+    @address = user.address.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @comment }
+    end
   end
 
   def new
+    user=User.find(params[:user_id])
+    @address = user.address.build
+
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @address }
+    end
   end
 
   def edit
+    user = User.find(params[:user_id])
+    @address = user.address.find(params[:id])
   end
 
-  def destroy
+  def update
+
+    user = User.find(params[:user_id])
+    @address = user.address.find(params[:id])
+
+    respond_to do |format|
+      if @address.update(address_params)
+        #1st argument of redirect_to is an array, in order to build the correct route to the nested resource address
+        format.html { redirect_to([@address.user, @address], :notice => 'Address was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @address.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy     
+    user = User.find(params[:user_id])     
+    @address = user.address.find(params[:id])
+    @address.destroy
+
+    respond_to do |format|     
+      format.html { redirect_to(user_address_url) }
+      format.xml  { head :ok }
+    end
+  end
+
+private
+
+  def address_params
+    params.require(:address).permit(:user_id, :address1, :address2, :city, :state_id, :zipcode)
   end
 end
